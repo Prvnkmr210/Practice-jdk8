fd529b64b596f6ecd25f8dbb7545867585a19e65
Praveen <abp.praveen@gmail.com>
2022-01-30 23:49:11 +0530

    Added filters

--- src/com/java/basics/streams/StreamIntegerIntoString.java
+++ src/com/java/basics/streams/StreamIntegerIntoString.java
@@ -16,9 +16,10 @@ public class StreamIntegerIntoString {
 		
 		
 		List<Integer> intList = new LinkedList<>();
-		Collections.addAll(intList, 5,10,25,8,36,58,16);
+		Collections.addAll(intList, 5,10,231,25,8,123,36,58,16,1234);
 		
 		System.out.println(intList.stream()
+				.filter(num -> num.toString().contains("1") && num.toString().contains("2") && num.toString().contains("3"))
 				.sorted()
 				.map(num -> String.valueOf(num))
 				.collect(Collectors.joining(",")));
