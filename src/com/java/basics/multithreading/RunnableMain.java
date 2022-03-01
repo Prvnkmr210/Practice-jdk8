d11da2447c02fa265f2282636a03a6f65677923a
Praveen <64293303+Prvnkmr210@users.noreply.github.com>
2022-03-01 20:29:29 +0530

    Added RunnableImplementation

--- src/com/java/basics/multithreading/RunnableMain.java
+++ src/com/java/basics/multithreading/RunnableMain.java
@@ -24,3 +24,24 @@ public class RunnableMain {
 	}
 
 }
+
+class RunnableImplementation implements Runnable{
+
+	private int threadNumber;
+
+
+	public RunnableImplementation(int i) {
+		this.threadNumber = i;
+	}
+
+	@Override
+	public void run() {
+		// all the codes within this block will be executed in multithread
+		for(int i=0; i<5;i++) {
+			System.out.println(i + " is priniting from Thread " + threadNumber);
+		}
+
+	}
+
+}
+
